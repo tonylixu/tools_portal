f7c6d904847305ce14d5ee4e95597ea145fd7e2e
tonylixu@gmail.com <tonylixu@gmail.com>
2017-05-03 10:21:15 -0400

    Define build task

--- gulpfile.js
+++ gulpfile.js
@@ -15,14 +15,14 @@ var uglify = require('gulp-uglify');
 var vulcanize = require('gulp-vulcanize');
 var del = require('del');
 
-/*
+/**
  * Define build task, it has a dependency on the default task
  */
 gulp.task('build', ['default'], function () {
   return gulp.src('[dist]').pipe(gulp.dest('_build'));
 });
 
-/*
+/**
  * Default Gulp task
  * 
  * Runs a clean (to clear out /build) and then builds the projects
@@ -30,4 +30,30 @@ gulp.task('build', ['default'], function () {
  */
 gulp.task('default', gulpSeq('clean', 'build'));
 
+/**
+ * Gulp clean task
+ * 
+ * Cleans out /build by deleting the entire folder using 'del'
+ */
+gulp.task('clean', function() {
+  return del(['dist']);
+});
 
+/**
+ * Gulp build task
+ * 
+ * Builds the project by doing the following:
+ * 1) Vulcanizing HTML imports by calling helper method
+ * 2) Move /admin folder to dist
+ * 3) Move all HTML files in /app to dist
+ * 4) Move images to dist
+ * 5) scripts to /dist
+ */
+gulp.task('build', ['vulcanize', 'moveAdmin', 'moveHTML', 'moveImg', 'moveScripts'], function (){
+
+});
+
+/** Gulp vulcanize task. Calls helper method to vulcanize HTML imports */
+gulp.task('vulcanize', function() {
+  return vulcanizeImports();
+});
