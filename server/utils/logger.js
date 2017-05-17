d905001425e5351d0b75f782848c8d9ae9f5df76
tonylixu@gmail.com <tonylixu@gmail.com>
2017-05-17 17:30:47 -0400

    Define different level of logging in loggers.js

--- server/utils/logger.js
+++ server/utils/logger.js
@@ -1,3 +1,44 @@
+/* Common logging functions */
+var winston = require('winston');
+
+// Add a debugging level logger
+winston.loggers.add('debug', {
+  console: {
+    level: 'debug',
+    colorize: true,
+  },
+
+  file: {
+    filename: './logs/debug.log'
+  }
+});
+
+// Add an error lever logger
+winston.loggers.add('error', {
+  console: {
+    level: 'error',
+    colorize: true,
+  },
+
+  file: {
+    filename: './logs/errors.log'
+  }
+});
+
+//Add an info level logger
+winston.loggers.add('info', {
+  console: {
+    level: 'info',
+    colorize: true,
+  },
+
+  file: {
+    filename: './logs/info.log'
+  }
+});
+
+// Export the loggers so we can use them throughout
+var debug = winston.loggers.get('debug');
 var error = winston.loggers.get('error');
 var info = winston.loggers.get('info');
 
