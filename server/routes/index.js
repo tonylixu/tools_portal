b6e98956540c4e25f87f8824a98cf96cc4939928
tonylixu@gmail.com <tonylixu@gmail.com>
2017-05-26 14:49:46 -0400

    Added index file on the server side

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,3 +1,16 @@
 // Top-Level Router
 // Mounts all the individual routes on a single router that the app can then use
 
+var router = require('express').Router();
+
+// Note: Each 'sub-router' should go below and use the following convention:
+// router.use(<PATH>, require(<MODULE>));
+// Where PATH = sub-path of main router
+// Where MODULE = the module containing the exported sub-router
+
+// Mount the 'news' router onto the API Router
+// URL will look like this: '.../api/news/...'
+router.use('/news', require('./news'));
+
+// !IMPORTANT! We need to export the router so our app can use the router
+module.exports = router;
